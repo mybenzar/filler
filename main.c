607d8393f0a2e01ad39263bf06e66fd72655191c
Myriam BENZARTI <mybenzar@student.42.fr>
2019-07-05 08:08:43 +0200

    small fix

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mybenzar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 15:21:14 by mybenzar          #+#    #+#             */
-/*   Updated: 2019/07/04 16:16:32 by mybenzar         ###   ########.fr       */
+/*   Updated: 2019/07/05 08:08:33 by mybenzar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,10 @@ int		main(void)
 	t_board	*board;
 	t_game	*game;
 	int		end;
+	static int turn = 0;
 
+	turn++;
+	dprintf(2, "turn = %d\n", turn);
 	end = 0;
 	if (!(board = (t_board*)ft_memalloc(sizeof(t_board)))
 		|| !(game = (t_game*)ft_memalloc(sizeof(t_game)))
@@ -40,9 +43,11 @@ int		main(void)
 				return (free_board(board) + free_game(game));
 			play(board, game);
 		}
-		end = 1;
+		else
+			end = 1;
 	}
 	free_board(board);
 	free_game(game);
+	dprintf(2, "hello\n");
 	return (1);
 }
